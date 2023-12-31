976c700d8f77c36a502b908305d3272bce39d130
minamimi <minamimimi>
2023-12-31 17:16:46 +0900

    Add

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,3 +1,7 @@
+#!/user/bin/python3
+# SPDX-FileCopyrightText: 2023 Saori Kitami　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
