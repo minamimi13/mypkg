6231c53b1489e979029b24f4131ee3414a391668
minamimi <minamimimi>
2023-12-31 19:59:54 +0900

    Add uranai

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -15,5 +15,14 @@ def generate_launch_description():
             executable='listener',
             output='screen'
             )
+    uranai_talker = launch_ros.actions.Node(
+            package='mypkg',
+            executable='uranai_talker',
+            )
+    uranai_listener = launch_ros.actions.Node(
+            package='mypkg',
+            executable='uranai_listener',
+            output='screen'
+            )
 
-    return launch.LaunchDescription([talker, listener])
+    return launch.LaunchDescription([talker, listener, uranai_talker, uranai_listener])
