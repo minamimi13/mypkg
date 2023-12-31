3ccc421eb981cec99354f5abfba5670870b9a04b
minamimi13 <147679325+minamimi13@users.noreply.github.com>
2023-12-31 23:55:52 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1" #引数があったら、そちらをホームに変える。
+[ "$1" != "" ] && dir="$1" # 引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-	grep 'Listen: 10'
+# talk_listen_launch.py のテスト
+timeout 10 ros2 launch mypkg talk_listen_launch.py > /tmp/mypkg.log
+
+# uranai_talk_listen_launch.py のテスト
+timeout 10 ros2 launch mypkg uranai_talk_listen_launch.py > /tmp/uranai_mypkg.log
+
+# どちらかのログに期待する文字列が含まれていれば 0 を返す
+if grep -q 'Listen: 10' /tmp/mypkg.log && grep -q '今日の運勢' /tmp/uranai_mypkg.log; then
+    echo 0
+else
+    echo 1
+fi
